8ee9f4a086b5f144fc9cd07b55553be70e0813ce
Chanchal185 <kmchanchal185@gmail.com>
2024-08-02 14:31:04 +0530

    Update ArithmeticOperatorInJava.java

--- OperatorsInJava.java
+++ OperatorsInJava.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class OperatorsInJava {
+public class ArithmeticOperatorInJava {
     public static void main(String[] args) {
         // using Airthmatic operator
         Scanner sc = new Scanner(System.in);
