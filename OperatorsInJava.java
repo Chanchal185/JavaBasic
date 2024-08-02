aef3ff2ab60b65679eb8ef6de9e088e01cea05ef
Chanchal185 <kmchanchal185@gmail.com>
2024-08-02 14:23:42 +0530

    Add files via upload

---
+++ OperatorsInJava.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class OperatorsInJava {
+    public static void main(String[] args) {
+        // using Airthmatic operator
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("sum="+sum);
+        int sub = a-b;
+        System.out.println("sub="+sub);
+        int mul = a*b;
+        System.out.println("mul="+mul);
+        float div = a/b;
+        System.out.println("div="+div);
+        int mod = a%b;
+        System.out.println("mod="+mod);
+        
+        sc.close();
+    }
+}
