c0c6ae47e932d0d21fa327d7904fd0887e9f4228
Chanchal185 <kmchanchal185@gmail.com>
2024-08-02 13:40:43 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        //taking input with scanner class
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        sc.close();
+    }
+    
+}
