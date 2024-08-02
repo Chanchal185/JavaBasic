926189cd217f5e4a30d3914d8e4ac1a2bb208f87
Chanchal185 <kmchanchal185@gmail.com>
2024-08-02 13:58:04 +0530

    Add files via upload

---
+++ DataTypeInJava.java
@@ -0,0 +1,21 @@
+public class DataTypeInJava {
+    public static void main(String[] args) {
+        //primitive Data types 
+        byte b = 127;
+        short s = 32767;
+        int i = 2147483647;
+        long l = 9223372036854775807L;
+        float f = 10.123456F;
+        double d = 10.123456789123456D;
+        boolean site = true;
+        char c = 'F';
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(site);
+        System.out.println(c);
+    }
+}
